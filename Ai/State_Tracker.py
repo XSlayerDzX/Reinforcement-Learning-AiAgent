198d338b5cdf4a937954c0deba3ed8b25904aa3f
SlayerDz <a.hissous@esi-sba.dz>
2026-02-26 15:24:52 +0100

    Refactor dataset creation logic and update match_id handling for improved output

--- Ai/State_Tracker.py
+++ Ai/State_Tracker.py
@@ -1,4 +1,5 @@
 Current_img = None
+Current_Id = None
 CurrentCard = None
 pos_x = None
 pos_y = None
@@ -8,3 +9,27 @@ CurrentTroops_ally = {}
 CurrentTroops_enemy = {}
 CurrentSlots = {}
 interrupt = False
+
+match_dict_input = {
+    "data" : [],
+}
+match_dict_output = {
+    "data" : [],
+}
+
+
+
+# import win32gui
+#
+# def enum_callback(hwnd, windows):
+#     if win32gui.IsWindowVisible(hwnd):
+#         title = win32gui.GetWindowText(hwnd)
+#         if title:  # Skip empty titles
+#             windows.append((hwnd, title))
+#     return True
+#
+# windows = []
+# win32gui.EnumWindows(enum_callback, windows)
+# for hwnd, title in windows:
+#     print(f"HWND: {hex(hwnd)}, Title: '{title}'")
+
